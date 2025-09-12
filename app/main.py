182e18e05ab6ac5fc7c045230b0ba884f3a197eb
stas <honcharenko.stas@gmail.com>
2025-09-12 14:28:04 +0100

    launch chrome

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
 import sys
 import os
+import time
 import shutil
+import subprocess
+import logging
 
 
 class App:
+    CHROME_CDP_PORT=9222 # http://localhost:9222 must be not in use on User's machine
+
     def __init__(self):
         chrome_path = self.find_chrome_executable()
-        print(chrome_path)
+        chrome_path = self.find_chrome_executable()
+        process = subprocess.Popen([
+            chrome_path,
+            f"--remote-debugging-port={self.CHROME_CDP_PORT}",
+            "--user-data-dir=/tmp/chrome-profile"
+        ])
+        time.sleep(5)
+        if process:
+            logging.info("Chrome launched")
+        else:
+            raise Exception("Chrome launching failed")
         
     @staticmethod
     def find_chrome_executable():
